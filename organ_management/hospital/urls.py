2b859d572f550865850ea08aec5afb34d6b1fa16
parampa10 <paramben10@gmail.com>
2021-01-29 23:51:45 +0530

    password reset done

--- organ_management/hospital/urls.py
+++ organ_management/hospital/urls.py
@@ -5,6 +5,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import PasswordResetView, PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView
 
 urlpatterns = [
     url('Mainpage/',Mainpage),
@@ -16,4 +17,9 @@ urlpatterns = [
     url(r'^registerhospital/',registerhospital),
     url(r'^loginhospital/',loginhospital),
     url(r'^logout/',logout),
+
+    path('password_reset/',PasswordResetView.as_view(template_name='password_reset.html'),name='password_reset'),
+	path('password_reset/complete/',PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),
+	path('password_reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),name='password_reset_confirm'),
+	path('password_reset/done/',PasswordResetDoneView.as_view(template_name='password_reset_done.html'),name='password_reset_done'),
 ]
--- organ_management/organ_management/settings.py
+++ organ_management/organ_management/settings.py
@@ -120,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'organmanagementsystem@gmail.com'
+EMAIL_HOST_PASSWORD = 'Organ@123'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
