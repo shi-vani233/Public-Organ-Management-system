c58baa57401c9686093b17622b78995e30081b61
devanshiks <68745733+devanshiks@users.noreply.github.com>
2021-02-03 18:24:20 +0530

    Search for Volunteers

--- organ_management/blood_donation/urls.py
+++ organ_management/blood_donation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blood_donation.views import BloodDonationHome,VolunteerPage,Registervol
-from django.conf.urls import url
+from blood_donation.views import BloodDonationHome,VolunteerPage,Registervol,searchBloodGroup
+from django.conf.urls import url,include
 
 urlpatterns = [
     url('BloodDonationHome/',BloodDonationHome),
     url('Volunteer/',VolunteerPage),
-    url(r'^Registervol/',Registervol)
+    url(r'^Registervol/',Registervol),
+    url(r'^searchBloodGroup/',searchBloodGroup),
 ]
--- organ_management/blood_donation/views.py
+++ organ_management/blood_donation/views.py
@@ -10,13 +10,15 @@ from django.contrib import auth
 from django.contrib import messages
 from django.core.mail import send_mail
 from blood_donation.models import Volunteer
+from django.db.models import Q
 
 # Create your views here.
 def BloodDonationHome(request):
     return render(request, 'BloodDonation.html', context=None)
 
 def VolunteerPage(request):
-    return render(request, 'volunteer.html', context=None)
+    volunteers=Volunteer.objects.all()
+    return render(request, 'volunteer.html',  {'volunteers':volunteers})
 
 def Registervol(request):
     volunteer_email=request.POST.get('volunteer_email','')
@@ -26,8 +28,43 @@ def Registervol(request):
     volunteer_bloodGroup=request.POST.get('volunteer_bloodGroup','')
     volunteer_gender=request.POST.get('volunteer_gender','')
     print(volunteer_email)
-    vol=Volunteer(volunteer_name=volunteer_name,volunteer_email=volunteer_email,volunteer_city=volunteer_city,
-    volunteer_mobile_no=volunteer_mobile_no,volunteer_bloodGroup=volunteer_bloodGroup,volunteer_gender=volunteer_gender)
-    vol.save()
-    msg="You are registered!"
-    return render(request,'volunteer.html',{'msg':msg})
+    volunteers=Volunteer.objects.all()
+    donor = Volunteer.objects.filter(volunteer_email=volunteer_email)
+    if len(volunteer_mobile_no)!=10:
+        msg_phone="phone number must be of length 10, please fill the form again with correct contact no"
+    else:
+        msg_phone=''
+    if donor.exists() :
+        msg_email = "This email is already registered"
+    else:
+        msg_email = ''
+    if not ( msg_phone or msg_email ):
+        vol=Volunteer(volunteer_name=volunteer_name,volunteer_email=volunteer_email,volunteer_city=volunteer_city,
+        volunteer_mobile_no=volunteer_mobile_no,volunteer_bloodGroup=volunteer_bloodGroup,volunteer_gender=volunteer_gender)
+        vol.save()
+        msg="You are registered!"
+        return render(request,'volunteer.html',{'msg':msg, 'volunteers':volunteers})
+    else:
+        return render(request ,'volunteer.html',{'msg_phone':msg_phone, 'msg_email':msg_email, 'volunteers':volunteers})
+
+def searchBloodGroup(request):
+    if request.method == 'GET':
+        query= request.GET.get('q')
+
+        submitbutton= request.GET.get('submit')
+
+        if query is not None:
+            lookups= Q(volunteer_bloodGroup__icontains=query) | Q(volunteer_city__icontains=query)
+
+            results= Volunteer.objects.filter(lookups).distinct()
+
+            context={'results': results,
+                     'submitbutton': submitbutton}
+
+            return render(request, 'volunteer.html', context)
+
+        else:
+            return render(request, 'volunteer.html')
+
+    else:
+        return render(request, 'volunteer.html')
